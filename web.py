d3a909c4fc2f70a34e486ada1ebf6dde259515d2
Thomas Povinelli <tompov227@gmail.com>
2020-05-25 00:22:26 -0400

    added types to web.py

--- web.py
+++ web.py
@@ -1,17 +1,18 @@
+from typing import List, Tuple
 import requests
 from bs4 import BeautifulSoup as bs
 
 BASE_URL = 'https://www.wordnik.com/words/{}'
 
 
-def get_page_source_for_word(word):
+def get_page_source_for_word(word: str) -> bytes:
     r = requests.get(BASE_URL.format(word))
     if r.status_code != 200:
         raise ConnectionError(r.reason)
     return r.content
 
 
-def get_all_definitions(html):
+def get_all_definitions(html: bytes) -> List[Tuple[str, str]]:
     tree = bs(html, 'html5lib')
     container = tree.find('div', id='define')
     definitions = []
